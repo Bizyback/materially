20218662b63bb1a6dbbffebd67b28d1fe6900f96
Mambo <mambobryan@gmail.com>
2024-03-16 23:18:03 +0300

    :tada: init canvas

---
+++ README.md
@@ -0,0 +1,4 @@
+<p align="center"><img width=100% src="/.github/images/materially.png" alt="Ding Logo"></p>
+
+# Materially
+This is a simple wrapper for Material3 components to be reused for both android mobile, tablet & tv
---
+++ materially/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.jetbrains.kotlin.android)
+}
+
+android {
+    namespace = "com.bizyback.materially"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation(platform(libs.compose.bom))
+    implementation(libs.bundles.compose)
+
+}
